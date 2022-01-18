b989b0e790d22af0125b21cfc047ac190e3747f8
shraddhapp <80465706+shraddhapp@users.noreply.github.com>
2022-01-18 18:46:37 +0530

    Time: 899 ms (95.22%), Space: 14.8 MB (40.67%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,33 +1,30 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        
         min_coin = [amount+1]*(amount+1)
         min_coin [0] = 0
-        
+        print(min_coin)
+        ''''
         for a in range(1, amount+1):
             for c in coins:
                 if a-c >=0:
                     min_coin[a] = min(min_coin[a], 1+min_coin[a-c])
         return min_coin[amount] if min_coin[amount] != amount+1 else -1
     
-    ''''
+        '''
         for i in range(1,(amount+1)):
             if i in coins:
-                print(i,"found in coins")
                 min_coin[i] = 1
             else:
-                print(i,"didnt found")
-                tempmin = amount
+                tempmin = amount+1
                 for coin in coins:
                     k = i-coin
                     if(k>0):
                         tot = 1+min_coin[k]
-                        print("for",i,":rem=",k,"for coin",coin,"needed values of",k,":",min_coin[k])
+                        #print("for",i,":rem=",k,"for coin",coin,"needed values of",k,":",min_coin[k])
                         if tot<tempmin:
                             tempmin=tot
                 min_coin[i] = tempmin
                 
-        print("\n----")
-        for i,j in enumerate(min_coin):
-            print(i,":",j,end= " ")
-        
-        return min_coin[amount]'''''
+      
+        return min_coin[amount] if min_coin[amount] != amount+1 else -1
